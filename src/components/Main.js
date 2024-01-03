29938efc8f18fa5b9e08f695273373a246af5984
Siddramappa <72513413+Developer-sidd@users.noreply.github.com>
2024-01-03 10:55:35 +0530

    Update Main.js

--- src/components/Main.js
+++ src/components/Main.js
@@ -150,7 +150,7 @@ const Main = () => {
           <span>click here</span>
         </Center>
 
-        <Contact target="_blank" href="mailto:codebucks27@gmail.com">
+        <Contact target="_blank" href="mailto:officialsidd.07@gmail.com">
           <motion.h2
             initial={{
               y: -200,
